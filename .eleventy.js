de4459cac58bc92cd8300bfc73906ef66ec89d0f
Jeff Siarto <jsiarto@gmail.com>
2024-02-05 15:18:52 +0000

    Add example shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("name", function(name, twitterusername) {
+    return `<div class="user">
+<div class="username">${name}</div>
+<div class="twitter">@${twitterusername}</div>
+</div>`;
+  });
 };
